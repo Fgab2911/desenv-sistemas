1146b360fd1859130b2503271c40ae82dc662cd5
Fernando Brun <fernandob.gabriel@gmail.com>
2025-07-29 09:34:35 -0300

    aula de laço 2

--- backend/logicaProgramacao/atividades.py
+++ backend/logicaProgramacao/atividades.py
@@ -5,3 +5,5 @@ for i in range(21):
     # soma= soma +i
 
     print(f'A soma dos números de 1 a 20 é: {soma}')
+
+
---
+++ backend/logicaProgramacao/atv2.py
@@ -0,0 +1,15 @@
+senha_correta = input("configure uma senha")
+senha = input("digite a senha")
+
+while senha != senha_correta:
+    print("senha invalida")
+    senha = input("digite a senha novamente")
+
+print("senha correta") 
+
+pedir nome e senha ao usuario 
+mostrar "bem vindo" quando acertar a senha e o nome
+após pedir o salario do usuario 
+mostrar salario anual 
+se o salario anual for maior que 100 mil mostrar mensagem "rico"
+

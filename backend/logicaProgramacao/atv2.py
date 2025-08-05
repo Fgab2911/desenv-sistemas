066954a88c0e850953e590c8136e1f7f87e6b7e1
Fernando Brun <fernandob.gabriel@gmail.com>
2025-08-05 09:47:42 -0300

    msg

--- backend/logicaProgramacao/atv2.py
+++ backend/logicaProgramacao/atv2.py
@@ -7,9 +7,26 @@ while senha != senha_correta:
 
 print("senha correta") 
 
-pedir nome e senha ao usuario 
-mostrar "bem vindo" quando acertar a senha e o nome
-após pedir o salario do usuario 
-mostrar salario anual 
-se o salario anual for maior que 100 mil mostrar mensagem "rico"
+#pedir nome e senha ao usuario 
+#mostrar "bem vindo" quando acertar a senha e o nome
+#após pedir o salario do usuario 
+#mostrar salario anual 
+#se o salario anual for maior que 100 mil mostrar mensagem "rico"
 
+senhacorreta = input("configure a senha")
+senha = input("digite a senha")
+salario = float(input("digite seu salario"))
+nome = input("digite o nome")
+
+while senha != senha_correta:
+    print("senha incorreta")
+    senha = input("digite a senha novamente")
+
+print("bem vindo")
+
+salarioanual = salario * 12
+if(salarioanual > 100.000)
+print("rico")
+
+else:
+    print("faz o L")
---
+++ backend/logicaProgramacao/atv3.py
@@ -0,0 +1,15 @@
+def saudacao(nome)
+print("olá", nome)
+
+saudacao("gabriel")
+def soma(num1, num2):
+    soma = num1 + num2
+    print("soma:", soma)
+
+    #crie uma função que faça a média de 3 valores
+    #crie uma função que calcule o imposto anual do seu salario
+    #crie uma função que valide se a senha está correta
+
+def calculomedia(valor1, valor2, valor3):
+    calculomedia = (valor1 + valor2 + valor3) / 3
+    print("sua media")

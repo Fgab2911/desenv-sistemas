cda3e3c141f5c00b0df1aff1793352858fc753aa
Fernando Brun <fernandob.gabriel@gmail.com>
2025-09-15 08:55:34 -0300

    msg

---
+++ backend/logicaProgramacao/dia08/atvnota3.py
@@ -0,0 +1,6 @@
+#Quando há mais de um atributo por registro, arquivos .txt simples se tornam difíceis de usar porque não têm estrutura fixa, exigem parsing manual, são 
+#propensos a erros e não escalam bem. Usar formatos estruturados como CSV, JSON ou bancos de dados é mais seguro e eficiente.
+
+
+# CSV: é um formato de arquivo de texto simples usado para armazenar dados tabulares, como uma planilha, onde os dados de cada coluna são separados por vírgulas (ou outro delimitador)
+#e as linhas representam os registros de dados

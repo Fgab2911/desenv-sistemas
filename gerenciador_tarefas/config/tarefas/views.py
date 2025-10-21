fddf7eda25bfbeb266fbea3b4d0bfc7af7ef6274
Fernando Brun <fernandob.gabriel@gmail.com>
2025-10-21 09:39:58 -0300

    msg

--- gerenciador_tarefas/config/tarefas/views.py
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 # Create your views here.
+ def detalhe_tarefa(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, pk=tarefa_id)
+    return render(request, 'tarefas/detalhe.html', {'tarefa': tarefa})

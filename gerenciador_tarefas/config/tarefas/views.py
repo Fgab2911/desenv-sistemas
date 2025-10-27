55ba440847ded60e0d7356e04004046dcd47007b
Fernando Brun <fernandob.gabriel@gmail.com>
2025-10-27 08:50:19 -0300

    msg

--- gerenciador_tarefas/config/tarefas/views.py
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import get_object_or_404, redirect, render
  def detalhe_tarefa(request, tarefa_id):
     tarefa = get_object_or_404(Tarefa, pk=tarefa_id)
     return render(request, 'tarefas/detalhe.html', {'tarefa': tarefa})
+
+def adicionar_tarefa(request):
+    if request.method == 'post':
+        titulo = request.post.get('titulo')
+        descricao = request.post.get('descricao')
+        Tarefa.objects.create(titulo=titulo, descricao=descricao)
+    return redirect('lista_tarefas')
